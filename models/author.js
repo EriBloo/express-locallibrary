5df075c6d8a77957c04fde9a42c4de13fcefe94f
eriBloo <19932449+EriBloo@users.noreply.github.com>
2021-01-05 16:51:07 +0100

    Added author and genre list

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -19,6 +20,18 @@ AuthorSchema
   .virtual('lifespan')
   .get(function() {
     return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  });
+
+AuthorSchema
+  .virtual('date_of_birth_formatted')
+  .get(function() {
+    return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+  });
+
+AuthorSchema
+  .virtual('date_of_death_formatted')
+  .get(function() {
+    return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
   });
 
 AuthorSchema
